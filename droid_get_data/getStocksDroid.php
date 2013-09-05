f5af9f419d8a2d7812d50285fdc735caa9950efe
Joshua Alday <aldajl@students.fscj.edu>
2013-09-05 23:29:13 +0900

    adding droid support file: Will retrieve files from database

---
+++ droid_get_data/getStocksDroid.php
@@ -0,0 +1,27 @@
+<?php
+function getDBCon($DB){
+	$con=mysqli_connect("pdb7.awardspace.net",$DB,"As84267139",$DB);
+	if (mysqli_connect_errno($con))
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		return false;
+	}
+	else{
+		return $con;
+	}
+}
+
+function getNasDaqData($conStocks){
+	$query = "SELECT stockName, stockStats, statDiff, statDiffPer, volume, beforeVal, afterVal, timestamp FROM nasdaqStats ORDER by PID DESC LIMIT 7";
+	$sendData = mysqli_query($conStocks, $query) or die('Error, nasdaq query failed');
+	return $sendData;
+}
+
+$conStocks = getDBCon("1491219_stocks");
+$stockData = getNasDaqData($conStocks);
+$output = array();
+while($data = mysqli_fetch_assoc($stockData)){
+	$output[] = $data;
+}
+print json_encode($output);
+?>
